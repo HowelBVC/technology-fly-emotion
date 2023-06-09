0ea0a2cc25b0c24ac62a92b196c0b607d6be210a
Howel Chan <howelvchan@gmail.com>
2023-06-09 15:05:37 -0400

    Update topbar design

--- src/components/topbar/Topbar.component.js
+++ src/components/topbar/Topbar.component.js
@@ -1,13 +1,61 @@
 import React from "react";
 import * as styles from "./Topbar.module.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+const navItems = [
+   {
+      label: "Home",
+      data: "home",
+   },
+   {
+      label: "Servizi",
+      data: "servizi",
+   },
+   {
+      label: "Realizzazioni",
+      data: "realizzazioni",
+   },
+   {
+      label: "Contattaci",
+      data: "contattaci",
+   },
+];
+
+const TopbarDesktop = () => {
+   const NavItem = ({ label }) => <Col><h4>{label}</h4></Col>
+
+   return (
+      <div className={`${styles.topbar} ${styles.desktop} align-items-center d-none d-lg-flex`}>
+         <Container>
+            <Row className="justify-content-between align-items-center">
+               {/* Logo Area */}
+               <Col lg="auto"><h1>Technology Fly Emotion</h1></Col>
+
+               {/* Nav Area */}
+               <Col lg={5}>
+                  <Row>
+                     {navItems.map(({ label }) => <NavItem label={label} />)}
+                  </Row>
+               </Col>
+            </Row>
+         </Container>
+      </div>
+   )
+}
+
+const TopbarMobile = () => {
+   return (
+      <div className={`${styles.topbar} ${styles.mobile} align-items-center justify-content-center d-flex d-lg-none`}>
+         <h4>Technology Fly Emotion</h4>
+      </div>
+   )
+}
 
 export default function Topbar() {
    return (
-      <div className={styles.topbar}>
-         <Container>
-            <h1>Technology Fly Emotion</h1>
-         </Container>
-      </div>
+      <React.Fragment>
+         <TopbarDesktop />
+         <TopbarMobile />
+      </React.Fragment>
    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import * as React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col } from "react-bootstrap";
 
 export default function MainPage() {
    return (
-      <Row>
-         <Col xs={6}>
-            <h1>Hello</h1>
-         </Col>
-         <Col xs={6}>
-            <h1>Hello</h1>
-         </Col>
-      </Row>
+      <React.Fragment />
    );
 }
 
