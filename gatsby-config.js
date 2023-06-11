1602fe74d7e99ff4362ce7a196c7e74490285dd8
Howel Chan <howelvchan@gmail.com>
2023-06-10 21:31:45 -0400

    Add Servizi Section

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Container, Row, Col } from "react-bootstrap";
 
 const BannerDesktop = () => (
-   <section className={`${styles.bannerDesktop} d-none d-lg-block`}>
+   <section className={`${styles.bannerDesktop} d-none d-md-block`}>
       <Container>
          <StaticImage src="../images/banner.png" alt="Technology Fly Emotion Banner" />
       </Container>
@@ -13,7 +13,7 @@ const BannerDesktop = () => (
 );
 
 const BannerMobile = () => (
-   <section className={`${styles.bannerMobile} d-block d-lg-none`}>
+   <section className={`${styles.bannerMobile} d-block d-md-none`}>
       <Container>
          <h1>Technology Fly Emotion</h1>
          <div className={styles.bannerButtonRow}>
@@ -37,7 +37,7 @@ const BannerMobile = () => (
 );
 
 const AboutUsDesktop = () => (
-   <section className={`${styles.aboutUsDesktop} d-none d-lg-block`}>
+   <section className={`${styles.aboutUsDesktop} d-none d-md-block`}>
       <Container>
          <h1>About Us</h1>
          <p>
@@ -52,13 +52,48 @@ const AboutUsDesktop = () => (
 )
 
 const AboutUsMobile = ({ children }) => (
-   <section className="d-block d-lg-none">
+   <section className="d-block d-md-none">
       <Container className={styles.aboutUsMobile}>
          <h1>About Us</h1>
          <p>
             Fly Emotion nasce nel 2010, lanciando sul mercato la sua prima attrazione - l'Aerofune® - nel luglio del 2011, che riscontra subito grande apprezzamento tra il pubblico (Tripadvisor), anche grazie al volo in coppia che viene reso possibile per la prima volta nel mondo.
          </p>
          <p>Scopri di piu'</p>
+      </Container>
+   </section>
+);
+
+const dataServizi = [
+   { label: "Ingegneria del territorio" },
+   { label: "Studi di fattibilita'" },
+   { label: "Progettazione integrata" }
+]
+
+const Servizi = () => (
+   <section className={styles.servizi}>
+      <Container className="text-start text-md-center">
+         <h2>I Nostri Servizi</h2>
+         <Row className={`${styles.serviziRow} flex-column flex-md-row px-2 px-md-0`}>
+            {dataServizi.map(({ label }, index) => (
+               <Col key={index} className={`${styles.serviziCard} mx-0 mx-md-2 my-2 my-md-0`}>
+                  <Row className="flex-row flex-md-column">
+                     {/* Image Area */}
+                     <Col className="order-1 order-md-0 px-0">
+                        <StaticImage
+                           src="../images/servizi-0.png"
+                           alt="Technology Fly Emotion"
+                           placeholder="blurred"
+                        />
+                     </Col>
+                     {/* Text Area */}
+                     <Col className={`${styles.serviziCardInner} order-0 order-md-1`}>
+                        <h4 className="d-none d-md-block">{label}</h4>
+                        <h3 className="d-block d-md-none">{label}</h3>
+                     </Col>
+                  </Row>
+               </Col>
+            ))}
+         </Row>
       </Container>
    </section>
 );
@@ -70,6 +105,7 @@ export default function MainPage() {
          <BannerMobile />
          <AboutUsDesktop />
          <AboutUsMobile />
+         <Servizi />
       </React.Fragment>
    );
 }
